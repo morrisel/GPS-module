ecbb30d5438c28a6bfc29666d8a7783792dcf961
morris <morris.elkanaev@gmail.com>
2024-12-01 03:08:19 +0200

    Uart_read function is implemented

--- RB/uart_RingBuffer.c
+++ RB/uart_RingBuffer.c
@@ -35,6 +35,8 @@ ring_buffer *_tx_buffer;
 /******************** Utility Functions ********************/
 static void Ringbuf_reset(ring_buffer *buffer);
 static void store_char(unsigned char c, ring_buffer *buffer);
+static int check_for(char *str, char *buffinder);
+
 
 void Ringbuf_init(void)
 {
@@ -84,12 +86,60 @@ static void Ringbuf_reset(ring_buffer *buffer)
     buffer->tail = 0;
 }
 
+/* checks if the entered string is present in the given buffer */
+static int check_for(char *str, char *buffinder)
+{
+    int slen = strlen(str);       // Length of the string to find
+    int blen = strlen(buffinder); // Length of the buffer
+    int so_far = 0;               // Number of characters matched so far
+    int indx = 0;                 // Current index in the buffer
+
+repeat:
+    while (str[so_far] != buffinder[indx]) // Look for the start of the match
+    {
+        indx++;
+        if (indx >= blen) return -1; // If we reached the end of the buffer, the string is not found
+    }
+
+    if (str[so_far] == buffinder[indx])
+    {
+        while (str[so_far] == buffinder[indx]) // Try to match the entire string
+        {
+            so_far++;
+            indx++;
+            if (so_far == slen) return 1; // String found
+        }
+    }
+
+    if (indx < blen)
+    {
+        so_far = 0; // If the string was not found, start from the next index
+        goto repeat; // Repeat the process from the new position in the buffer
+    }
+
+    return -1; // If the string is not found
+}
 
 
 
+int Uart_read(void)
+{
+    // If the head isn't ahead of the tail, there are no characters available
+    if (_rx_buffer->head == _rx_buffer->tail)
+    {
+        return -1; // No data available
+    }
+    else
+    {
+        // Read the data from the buffer at the tail position
+        unsigned char c = _rx_buffer->buffer[_rx_buffer->tail];
 
+        // Increment the tail index, wrapping around if necessary
+        _rx_buffer->tail = (_rx_buffer->tail + 1) % UART_BUFFER_SIZE;
 
-
-
+        // Return the read character
+        return c;
+    }
+}
 
 
